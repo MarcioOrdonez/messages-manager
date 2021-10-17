a1ef90951b1c0d5b3e6e966f7db2eee76752a5c5
Marcio Ordonez <marcio.v.ordonez@gmail.com>
2021-10-17 15:24:35 -0300

    feat(repositories): add twitter message repository implementation

---
+++ server/repositories/twitter/repository.ts
@@ -0,0 +1,25 @@
+import { Filters, MessageRepository } from "../../domains/ports";
+import { twitterApi } from "./services";
+
+export interface TwitterRule {
+  id: string;
+  value: string;
+}
+
+export class TwitterRepository implements MessageRepository {
+  public async setFilters(filters: Filters): Promise<void> {
+    await this.clearRules();
+    await twitterApi.setRules(filters);
+  }
+
+  public async getMessageStream(): Promise<NodeJS.ReadableStream> {
+    return await twitterApi.stream();
+  }
+
+  private async clearRules(): Promise<void> {
+    const rules = await twitterApi.getRules();
+    const ids = rules.map(({ id }: TwitterRule) => id);
+    console.log(ids);
+    await twitterApi.clearRules(ids);
+  }
+}
---
+++ server/repositories/twitter/services.ts
@@ -0,0 +1,51 @@
+import needle from "needle";
+import { Filters } from "../../domains/ports";
+import { TwitterRule } from "./repository";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const RULESURL = process.env.TWITTER_RULE;
+const STREAMURL = process.env.TWITTER_STREAM;
+const TOKEN = process.env.TWITTER_TOKEN;
+
+export const twitterApi = {
+  async setRules(filters: Filters): Promise<void> {
+    const data = { add: filters };
+    await needle("post", RULESURL, data, {
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${TOKEN}`,
+      },
+    });
+  },
+
+  async getRules(): Promise<Array<TwitterRule>> {
+    const response = await needle("get", RULESURL, {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+      },
+    });
+    if (response.body.meta.result_count === 0) return [];
+    return response.body.data;
+  },
+
+  async clearRules(ids: Array<string>): Promise<void> {
+    const data = { delete: { ids: ids } };
+    await needle("post", RULESURL, data, {
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${TOKEN}`,
+      },
+    });
+  },
+
+  async stream(): Promise<NodeJS.ReadableStream> {
+    const stream = await needle.get(STREAMURL, {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+      },
+    });
+    return stream;
+  },
+};
