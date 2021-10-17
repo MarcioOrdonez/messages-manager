160b4e57201b7ebb626a493d7c54db2e130da403
Marcio Ordonez <marcio.v.ordonez@gmail.com>
2021-10-17 15:08:32 -0300

    feat(domain): add message related interfaces and class

---
+++ server/domains/messages.ts
@@ -0,0 +1,29 @@
+interface Users {
+  id: string;
+  name: string;
+  username: string;
+}
+export interface MessageData {
+  text: string;
+  author_id: string;
+  users: Users;
+}
+
+export class Message {
+  private message: MessageData;
+
+  constructor(data: MessageData) {
+    this.message = { text: data.text, author_id: data.author_id, users: data.users };
+  }
+
+  public getText(): string {
+    return this.message.text;
+  }
+
+  public getAuthor(): string {
+    return this.message.author_id;
+  }
+  public getUser(): Users {
+    return this.message.users;
+  }
+}
