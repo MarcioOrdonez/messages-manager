1ed5376c6b51ab75735a435b8c5e80f486a17022
Marcio Ordonez <marcio.v.ordonez@gmail.com>
2021-10-17 21:06:37 -0300

    feat(client): add link buttons

--- client/src/components/NavBar/index.tsx
+++ client/src/components/NavBar/index.tsx
@@ -1,4 +1,5 @@
 import "./styles.css";
+import { Link } from "react-router-dom";
 import type { FC } from "react";
 
 
@@ -7,8 +8,8 @@ import type { FC } from "react";
 const NavBar: FC = () => (
     <div className="nav-bar">
         <div className="nav-bar__buttons">
-            <div className="nav-bar__manage-page" onClick={()=>{}}></div>
-            <div className="nav-bar__view-page" onClick={()=>{}}></div>
+        <div className="nav-bar__manage-page"><Link to="/">Gerenciar mensagens</Link></div>
+        <div className="nav-bar__view-page"><Link to="/view">Receber mensagens</Link></div>
         </div>
     </div>
 )
